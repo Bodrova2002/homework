c898ab0efd2338da190d110b6c999d4820bae32c
Bodrova2002 <71357142+Bodrova2002@users.noreply.github.com>
2020-10-01 11:54:50 +0300

    Add files via upload

---
+++ Log.java
@@ -0,0 +1,10 @@
+public class Log {
+    public static void main(String[] args){
+        log("In main method");
+    }
+    public static void log(String s){
+        String nameClass = Thread.currentThread().getStackTrace()[2].getClassName();
+        String nameMethod = Thread.currentThread().getStackTrace()[2].getMethodName();
+        System.out.println("Your.packet." + nameClass + ": " + nameMethod + ": " + s);
+    }
+}
